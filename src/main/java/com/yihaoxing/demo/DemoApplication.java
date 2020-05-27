9bd6d3c560289923b24a450122c7773e8a4b672d
yihaoxing <haoxingyi@91jkys.com>
2020-05-27 19:08:47 +0800

    First Commit

---
+++ src/main/java/com/yihaoxing/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.yihaoxing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YiHaoxing
+ * @date 2020/5/27 18:47
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
