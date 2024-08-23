486884c2baf2f6248c388de10a4f517ea1fda3fa
yihaoxing <m15670388498@163.com>
2024-08-23 10:42:20 +0800

    1

--- src/test/java/com/yihaoxing/demo/test/serialize/SerializeTest.java
+++ src/test/java/com/yihaoxing/demo/test/serialize/SerializeTest.java
@@ -118,4 +118,44 @@ public class SerializeTest {
     }
 
 
+    /**
+     * 测试循环依赖的对象使用jdk序列化会不会报错
+     * 如果没有重写toString、hashCode这些方法（因为这些方法会用到循环引用的对象），是不会报错的
+     * 所以jdk的序列化方式，就是类信息+数据的地址，他不会去遍历每个类变量，所以就不会导致序列化和反序列化时栈内存溢出
+     *
+     * 如果加上toString、hashCode这些方法，或者使用json的序列化方式，就会有栈内存溢出的问题
+     */
+    @Test
+    public void test06() {
+
+        FileOutputStream out;
+        ObjectOutputStream oout;
+        FileInputStream in;
+        ObjectInputStream oin;
+        try {
+
+            C parent = new C();
+            parent.setS("fff");
+
+            C child = new C();
+            child.setS("abc");
+            child.setParent(parent);
+            parent.setChild(child);
+
+            out = new FileOutputStream("c1.txt");//序列化
+            oout = new ObjectOutputStream(out);
+            oout.writeObject(parent);
+            oout.close();
+
+            in = new FileInputStream("c1.txt");
+            oin = new ObjectInputStream(in);
+            C c11 = (C) oin.readObject();
+            System.out.println(c11.getChild().getS());
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
 }
