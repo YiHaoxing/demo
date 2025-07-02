f854d7b1bb6db58ae442c78b7f27d7f88e734e22
yihaoxing <m15670388498@163.com>
2025-07-02 16:33:23 +0800

    1

--- src/main/java/com/yihaoxing/demo/DemoApplication.java
+++ src/main/java/com/yihaoxing/demo/DemoApplication.java
@@ -2,12 +2,14 @@ package com.yihaoxing.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author YiHaoxing
  * @date 2020/5/27 18:47
  */
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
